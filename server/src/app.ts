9eacc73749b36613e019a44be47196d938aadb6d
Kamui <fin-kamui@pm.me>
2022-07-07 13:55:06 -0500

    feat: introduce and structure SCSS

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,11 +2,12 @@ import express from "express"
 import helmet from "helmet"
 import cors from "cors"
 import morgan from "morgan"
-import { APP_PORT } from "./config"
+import { AppDataSource, APP_PORT } from "./config"
 import { errorHandler } from "./middlewares/error"
 import { errors } from "celebrate"
 import { router } from "./routes"
 import { logger } from "./utils"
+import "reflect-metadata"
 
 const app = express()
 app.use(cors({ origin: "http://localhost:3000", credentials: true }))
@@ -17,9 +18,13 @@ app.use(router)
 app.use(errors())
 app.use(errorHandler)
 
-app.listen(APP_PORT, () => {
-    logger.info(`Listening to port ${APP_PORT}`)
-})
+AppDataSource.initialize()
+    .then(() => {
+        app.listen(APP_PORT, () => {
+            logger.info(`Listening to port ${APP_PORT}`)
+        })
+    })
+    .catch((error) => console.log(error))
 
 process.on("unhandledRejection", (reason: Error) => {
     throw reason
--- server/src/config.ts
+++ server/src/config.ts
@@ -1,9 +1,28 @@
 import dotenv from "dotenv"
+import { DataSource } from "typeorm"
 
 dotenv.config()
 
 export const {
     NODE_ENV = "development",
     APP_PORT = 3000,
+    POSTGRES_HOST = "localhost",
+    POSTGRES_PORT = 5432,
+    POSTGRES_USERNAME = "postgres",
+    POSTGRES_PASSWORD = "postgres",
+    POSTGRES_DB_NAME = "mme",
 } = process.env
 
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: POSTGRES_HOST,
+    port: POSTGRES_PORT,
+    username: POSTGRES_USERNAME,
+    password: POSTGRES_PASSWORD,
+    database: POSTGRES_DB_NAME,
+    synchronize: true,
+    logging: true,
+    entities: [],
+    subscribers: [],
+    migrations: [],
+})
