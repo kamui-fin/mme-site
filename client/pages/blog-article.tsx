a4707c0899a5c21326344227b67c184af636c06d
Kamui <fin-kamui@pm.me>
2022-07-08 15:33:22 -0500

    feat: initialize components

--- client/pages/blog-article.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from 'next'
-
-const Home: NextPage = () => {
-    return (
-    <div>Blog Article Page</div>
-    )
-}
-
-export default Home
--- client/pages/product-details.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from 'next'
-
-const Home: NextPage = () => {
-    return (
-    <div>Product Details Page</div>
-    )
-}
-
-export default Home
