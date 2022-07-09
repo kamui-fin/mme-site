82d2287ceb8b9467df69067c5d86e704c01af128
Kamui <fin-kamui@pm.me>
2022-07-09 11:49:38 -0500

    feat: add fade-out description component

---
+++ client/components/Description.tsx
@@ -0,0 +1,21 @@
+import cx from "classnames"
+import { useState } from "react"
+import styles from "scss/components/Description.module.scss"
+
+interface Props {
+    desc: string
+}
+
+export const Description = ({ desc }: Props) => {
+    const [collapsed, setCollapsed] = useState(true)
+    return (
+        <>
+            <div className={cx(styles.container, { [styles.collapsed]: collapsed })}>
+                <span className={styles.description}>{desc}</span>
+            </div>
+            <p className={styles.readMore} onClick={() => setCollapsed(!collapsed)}>
+                Read more
+            </p>
+        </>
+    )
+}
