803798cb480aa556221130f128fc04e5a1a17b4d
Kamui <fin-kamui@pm.me>
2022-07-09 10:27:01 -0500

    feat: add select component

---
+++ client/components/Select.tsx
@@ -0,0 +1,29 @@
+import cx from "classnames"
+import styles from "scss/components/Select.module.scss"
+
+interface Props {
+    icon?: React.ReactNode
+    onDone?: (text: string) => void
+    items: string[]
+    defaultIndex?: number
+}
+
+export const Select = ({ icon, onDone, items, defaultIndex = 0 }: Props) => {
+    return (
+        <div className={styles.container}>
+            {icon && <div className={styles.iconBox}>{icon}</div>}
+            <select
+                className={cx(styles.select, { [styles.withIcon]: !!icon })}
+                onChange={(ev) => {
+                    onDone && onDone(items[ev.target.value])
+                }}
+            >
+                {items.map((item, indx) => (
+                    <option className={styles.item} value={indx} selected={defaultIndex === indx}>
+                        {item}
+                    </option>
+                ))}
+            </select>
+        </div>
+    )
+}
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,12 @@
 import { Input } from "components/Input"
-import SearchIcon from "assets/search.svg"
+import SortIcon from "assets/sort.svg"
 import type { NextPage } from "next"
+import { Select } from "components/Select"
 
 const Home: NextPage = () => {
     return (
         <div>
-            <Input placeholder="Search" icon={<SearchIcon />} />
+            <Select icon={<SortIcon />} items={["Popular", "Trending", "Best"]} defaultIndex={1} onDone={(txt) => console.log(txt)}/>
         </div>
     )
 }
