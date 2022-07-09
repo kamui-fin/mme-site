c2e1a30ea291413e49738682a4eebd069f523c3c
Kamui <fin-kamui@pm.me>
2022-07-09 18:55:56 -0500

    feat: add product schema

--- client/components/Carousel.tsx
+++ client/components/Carousel.tsx
@@ -1,68 +1,94 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination, Navigation } from "swiper";
-import 'swiper/css';
-import style from "scss/components/Carousel.module.scss";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import { BookCard } from "./BookCard";
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Pagination, Navigation } from "swiper"
+import cx from "classnames"
+import "swiper/css"
+import style from "scss/components/Carousel.module.scss"
+import "swiper/css"
+import "swiper/css/pagination"
+import "swiper/css/navigation"
+import { BookCard } from "./BookCard"
 
 interface Props {
-  text: string
+    className?: string
 }
 
-export const Carousel = ({text}: Props) => {
-    const cards = [{
-      title: "Sword Art Online Vol. 1", author: "Reki Kawahara", image: "https://images-na.ssl-images-amazon.com/images/I/51UVCfCXmzL._SX331_BO1,204,203,200_.jpg", coverType: "Paperback", price: 12.99
-    },
-    {
-      title: "Sword Art Online Vol. 1", author: "Reki Kawahara", image: "https://images-na.ssl-images-amazon.com/images/I/51UVCfCXmzL._SX331_BO1,204,203,200_.jpg", coverType: "Paperback", price: 12.99
-    },
-    {
-      title: "Sword Art Online Vol. 1", author: "Reki Kawahara", image: "https://images-na.ssl-images-amazon.com/images/I/51UVCfCXmzL._SX331_BO1,204,203,200_.jpg", coverType: "Paperback", price: 12.99
-    },
-    {
-      title: "Sword Art Online Vol. 1", author: "Reki Kawahara", image: "https://images-na.ssl-images-amazon.com/images/I/51UVCfCXmzL._SX331_BO1,204,203,200_.jpg", coverType: "Paperback", price: 12.99
-    },
-    {
-      title: "Sword Art Online Vol. 1", author: "Reki Kawahara", image: "https://images-na.ssl-images-amazon.com/images/I/51UVCfCXmzL._SX331_BO1,204,203,200_.jpg", coverType: "Paperback", price: 12.99
-    },
-    {
-      title: "Sword Art Online Vol. 1", author: "Reki Kawahara", image: "https://images-na.ssl-images-amazon.com/images/I/51UVCfCXmzL._SX331_BO1,204,203,200_.jpg", coverType: "Paperback", price: 12.99
-    }];
+export const Carousel = ({ className }: Props) => {
+    const cards = [
+        {
+            title: "Sword Art Online Vol. 1",
+            author: "Reki Kawahara",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51UVCfCXmzL._SX331_BO1,204,203,200_.jpg",
+            coverType: "Paperback",
+            price: 12.99,
+        },
+        {
+            title: "Sword Art Online Vol. 1",
+            author: "Reki Kawahara",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51UVCfCXmzL._SX331_BO1,204,203,200_.jpg",
+            coverType: "Paperback",
+            price: 12.99,
+        },
+        {
+            title: "Sword Art Online Vol. 1",
+            author: "Reki Kawahara",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51UVCfCXmzL._SX331_BO1,204,203,200_.jpg",
+            coverType: "Paperback",
+            price: 12.99,
+        },
+        {
+            title: "Sword Art Online Vol. 1",
+            author: "Reki Kawahara",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51UVCfCXmzL._SX331_BO1,204,203,200_.jpg",
+            coverType: "Paperback",
+            price: 12.99,
+        },
+        {
+            title: "Sword Art Online Vol. 1",
+            author: "Reki Kawahara",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51UVCfCXmzL._SX331_BO1,204,203,200_.jpg",
+            coverType: "Paperback",
+            price: 12.99,
+        },
+        {
+            title: "Sword Art Online Vol. 1",
+            author: "Reki Kawahara",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51UVCfCXmzL._SX331_BO1,204,203,200_.jpg",
+            coverType: "Paperback",
+            price: 12.99,
+        },
+    ]
 
-    const listCards = cards.map((card) =>
-      <SwiperSlide>
-        <div className={style.carouselItem}>
-          <BookCard 
-            title={card.title} 
-            author={card.author} 
-            image={card.image} 
-            coverType={"Paperback"} 
-            price={card.price}
-          />
-        </div>
-      </SwiperSlide>
-    );
-    
+    const listCards = cards.map((card) => (
+        <SwiperSlide>
+            <div className={style.carouselItem}>
+                <BookCard
+                    title={card.title}
+                    author={card.author}
+                    image={card.image}
+                    coverType={"Paperback"}
+                    price={card.price}
+                />
+            </div>
+        </SwiperSlide>
+    ))
+
     return (
-      <>
-      <h1 className={style.carouselText}>{text}</h1>
-      <Swiper
-      className={style.swiper}
-      slidesPerView={5}
-      slidesPerGroup={1}
-      loop={true}
-      navigation
-      centeredSlides={true}
-      loopFillGroupWithBlank={true}
-      pagination={{
-        enabled: false
-      }}
-      modules={[Pagination, Navigation]}
-    >
-      {listCards}
-    </Swiper>
-    </>
+        <>
+            <Swiper
+                className={cx(style.swiper, className)}
+                slidesPerView={4}
+                slidesPerGroup={1}
+                loop={true}
+                navigation
+                centeredSlides={true}
+                loopFillGroupWithBlank={true}
+                pagination={{
+                    enabled: false,
+                }}
+                modules={[Pagination, Navigation]}
+            >
+                {listCards}
+            </Swiper>
+        </>
     )
 }
