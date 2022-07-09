d72f68a3bd9c35f0996497a584b1b89efaa8e388
Kamui <fin-kamui@pm.me>
2022-07-09 13:25:49 -0500

    feat: add about page

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -1,7 +1,16 @@
 import type { NextPage } from "next"
+import styles from "scss/layouts/about.module.scss"
 
 const About: NextPage = () => {
-    return <div>About Us Page</div>
+    return (
+        <div className={styles.aboutContainer}>
+            <h1 className={styles.title}>About Us</h1>
+            <h3 className={styles.subtitle}>Our mission</h3>
+            <p className={styles.para}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h3 className={styles.subtitle}>Our story</h3>
+            <p className={styles.para}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+    )
 }
 
 export default About
