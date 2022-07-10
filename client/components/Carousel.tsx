7670c5df7068db877a8c70f7bb3c1c02b66595aa
Luck <louisdelozier@gmail.com>
2022-07-09 21:18:19 -0500

    Added Genre Carousel and Modified the Component

--- client/components/Carousel.tsx
+++ client/components/Carousel.tsx
@@ -6,77 +6,19 @@ import style from "scss/components/Carousel.module.scss"
 import "swiper/css"
 import "swiper/css/pagination"
 import "swiper/css/navigation"
-import { BookCard } from "./BookCard"
 
 interface Props {
     className?: string
+    count: number
+    children: React.ReactNode
 }
 
-export const Carousel = ({ className }: Props) => {
-    const cards = [
-        {
-            title: "Sword Art Online Vol. 1",
-            author: "Reki Kawahara",
-            image: "https://images-na.ssl-images-amazon.com/images/I/51UVCfCXmzL._SX331_BO1,204,203,200_.jpg",
-            coverType: "Paperback",
-            price: 12.99,
-        },
-        {
-            title: "Sword Art Online Vol. 1",
-            author: "Reki Kawahara",
-            image: "https://images-na.ssl-images-amazon.com/images/I/51UVCfCXmzL._SX331_BO1,204,203,200_.jpg",
-            coverType: "Paperback",
-            price: 12.99,
-        },
-        {
-            title: "Sword Art Online Vol. 1",
-            author: "Reki Kawahara",
-            image: "https://images-na.ssl-images-amazon.com/images/I/51UVCfCXmzL._SX331_BO1,204,203,200_.jpg",
-            coverType: "Paperback",
-            price: 12.99,
-        },
-        {
-            title: "Sword Art Online Vol. 1",
-            author: "Reki Kawahara",
-            image: "https://images-na.ssl-images-amazon.com/images/I/51UVCfCXmzL._SX331_BO1,204,203,200_.jpg",
-            coverType: "Paperback",
-            price: 12.99,
-        },
-        {
-            title: "Sword Art Online Vol. 1",
-            author: "Reki Kawahara",
-            image: "https://images-na.ssl-images-amazon.com/images/I/51UVCfCXmzL._SX331_BO1,204,203,200_.jpg",
-            coverType: "Paperback",
-            price: 12.99,
-        },
-        {
-            title: "Sword Art Online Vol. 1",
-            author: "Reki Kawahara",
-            image: "https://images-na.ssl-images-amazon.com/images/I/51UVCfCXmzL._SX331_BO1,204,203,200_.jpg",
-            coverType: "Paperback",
-            price: 12.99,
-        },
-    ]
-
-    const listCards = cards.map((card) => (
-        <SwiperSlide>
-            <div className={style.carouselItem}>
-                <BookCard
-                    title={card.title}
-                    author={card.author}
-                    image={card.image}
-                    coverType={"Paperback"}
-                    price={card.price}
-                />
-            </div>
-        </SwiperSlide>
-    ))
-
+export const Carousel = ({ className, count, children }: Props) => {
     return (
         <>
             <Swiper
                 className={cx(style.swiper, className)}
-                slidesPerView={4}
+                slidesPerView={count | 3}
                 slidesPerGroup={1}
                 loop={true}
                 navigation
@@ -87,7 +29,7 @@ export const Carousel = ({ className }: Props) => {
                 }}
                 modules={[Pagination, Navigation]}
             >
-                {listCards}
+                {children}
             </Swiper>
         </>
     )
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,19 +1,117 @@
 import type { NextPage } from "next"
 import { Hero } from "components/Hero"
 import styles from "scss/layouts/index.module.scss"
+import carousel from "scss/components/Carousel.module.scss"
 import { Carousel } from "components/Carousel"
+import { SwiperSlide } from "swiper/react"
+import { BookCard } from "components/BookCard"
+import ScifiIcon from "assets/scifi.svg"
+import { IconTextCard } from "components/IconTextCard"
 
 const Home: NextPage = () => {
+    const cards = [
+        {
+            title: "Sword Art Online Vol. 1",
+            author: "Reki Kawahara",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51UVCfCXmzL._SX331_BO1,204,203,200_.jpg",
+            coverType: "Paperback",
+            price: 12.99,
+        },
+        {
+            title: "Sword Art Online Vol. 1",
+            author: "Reki Kawahara",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51UVCfCXmzL._SX331_BO1,204,203,200_.jpg",
+            coverType: "Paperback",
+            price: 12.99,
+        },
+        {
+            title: "Sword Art Online Vol. 1",
+            author: "Reki Kawahara",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51UVCfCXmzL._SX331_BO1,204,203,200_.jpg",
+            coverType: "Paperback",
+            price: 12.99,
+        },
+        {
+            title: "Sword Art Online Vol. 1",
+            author: "Reki Kawahara",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51UVCfCXmzL._SX331_BO1,204,203,200_.jpg",
+            coverType: "Paperback",
+            price: 12.99,
+        },
+        {
+            title: "Sword Art Online Vol. 1",
+            author: "Reki Kawahara",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51UVCfCXmzL._SX331_BO1,204,203,200_.jpg",
+            coverType: "Paperback",
+            price: 12.99,
+        },
+        {
+            title: "Sword Art Online Vol. 1",
+            author: "Reki Kawahara",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51UVCfCXmzL._SX331_BO1,204,203,200_.jpg",
+            coverType: "Paperback",
+            price: 12.99,
+        },
+    ]
+    const genres=[
+        {
+            icon: <ScifiIcon/>,
+            text: "Sci-Fi"
+        },
+        {
+            icon: <ScifiIcon/>,
+            text: "Sci-Fi"
+        },
+        {
+            icon: <ScifiIcon/>,
+            text: "Sci-Fi"
+        },
+        {
+            icon: <ScifiIcon/>,
+            text: "Sci-Fi"
+        },
+        {
+            icon: <ScifiIcon/>,
+            text: "Sci-Fi"
+        }
+    ]
+
+    const listGenres = genres.map((genre) => (
+        <SwiperSlide>
+            <div className={carousel.carouselItem}>
+                <IconTextCard
+                  icon={genre.icon}
+                  text={genre.text}
+                />
+            </div>
+        </SwiperSlide>
+    ))
+    
+    const listCards = cards.map((card) => (
+        <SwiperSlide>
+            <div className={carousel.carouselItem}>
+                <BookCard
+                    title={card.title}
+                    author={card.author}
+                    image={card.image}
+                    coverType={"Paperback"}
+                    price={card.price}
+                />
+            </div>
+        </SwiperSlide>
+    ))
+
     return (
         <>
             <Hero />
             <div className={styles.bottomLanding}>
                 <section className={styles.books}>
                     <h3 className={styles.subtitle}>Book Selection</h3>
-                    <Carousel className={styles.bookCarousel}/>
+                    <Carousel count={5} className={styles.bookCarousel} children={listCards}/>
                 </section>
                 <section className={styles.genre}>
                     <h3 className={styles.subtitle}>Genres</h3>
+                    <Carousel count={4} children={listGenres}/>
                 </section>
             </div>
         </>
