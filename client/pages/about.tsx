efe920fe0838465f649d42242bcd1bcfcdd657ac
Luck <louisdelozier@gmail.com>
2022-07-11 11:50:44 -0500

    Made About Us Page Responsive

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -1,7 +1,7 @@
 import BreadCrumbs from "components/BreadCrumbs"
 import Button from "components/Button"
 import type { NextPage } from "next"
-import BookLover from "assets/book-lover.svg"
+import Image from 'next/image'
 import styles from "scss/layouts/about.module.scss"
 import Link from "next/link"
 
@@ -14,7 +14,7 @@ const About: NextPage = () => {
             <p className={styles.para}>We are a team of various people who have sought to bridge the gap between languages and cultures and bring awesome new titles, too often neglected by other publishing companies in Spain. Our task is to improve the catalogue of books avaiable in Spanish and to provide a service where there's a demand for it.</p>
             <Link href="/contact"><a><Button className={styles.contactBtn} btnType="primary">Get in touch</Button></a></Link>
             <div className={styles.img}>
-                <BookLover />
+                <Image src="/book-lover.png" width={669} height={512}/>
             </div>
         </div>
     )
